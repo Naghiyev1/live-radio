42c7351bb69e0a7347a1707fcebfa71403b34b85
Naghiyev1 <hnaghiyev@gmail.com>
2021-05-18 14:47:21 +0100

    added necessary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Radio from './Radio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <h1>Live-Radio Player</h1>
+    <h2>Pick a genre, choose a station, start listening</h2>
+    <Radio />
+  </div>
   );
 }
 
